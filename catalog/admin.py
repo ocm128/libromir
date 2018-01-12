68690a6b2e58c5eb3e26f714830dd3a260d1aa9f
kalasni <kalassni@gmail.com>
2018-01-12 19:00:05 +0100

    Created and registered models and made migration

---
+++ catalog/admin.py
@@ -0,0 +1,59 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+
+class BooksInline(admin.TabularInline):
+
+    """
+    Defines format of inline book insertion (used in AuthorAdmin)
+    """
+    model = Book
+    extra = 0
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BooksInline]
+
+class BooksInstanceInline(admin.TabularInline):
+    """
+    Defines format of inline book instance insertion (used in BookAdmin)
+    """
+    model = BookInstance
+    extra = 0  # Show "Add another Book instance" link
+
+
+# Register the Admin classes for Book using the decorator
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+
+    list_display = ('book', 'status','due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+# Register your models here.
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+admin.site.register(Language)
+#admin.site.register(Book)
+#admin.site.register(BookInstance)
+
