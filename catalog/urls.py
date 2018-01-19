b6e9d83326c01441df7ad936c231a204c9e1f957
kalasni <kalassni@gmail.com>
2018-01-19 21:34:20 +0100

    Added form and view renew_book_librarian

--- catalog/urls.py
+++ catalog/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     url(r'author/(?P<pk>[\w\-]+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     url(r'mybooks', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     url(r'borrowed', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+    url(r'book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
 ]
