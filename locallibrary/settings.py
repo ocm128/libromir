3b4896c1683416a65628919dd69c55bd39c0edd7
kalasni <kalassni@gmail.com>
2018-01-25 20:55:04 +0100

    Modified settings.py file

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db_library',
-        'USER': 'userlibrary',
-        'PASSWORD': 'pass',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
